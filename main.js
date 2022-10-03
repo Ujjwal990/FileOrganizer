2b82289903d33c8b01e8549764398dbd7367f1f5
Ujjwal Kumar <ujjwalkumar080302@gmail.com>
2022-10-03 18:00:18 +0530

    final changes done

---
+++ main.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+let inputArr = process.argv.slice(2);
+let fs = require("fs");
+let path = require("path");
+let helpObj = require("./commands/help");
+let treeObj = require("./commands/tree");
+let organizeObj = require("./commands/organize");
+
+// console.log(inputArr);
+// node main.js tree "directoryPath"
+// node main.js organize "directoryPath"
+// node main.js help
+let command = inputArr[0];
+
+switch (command) {
+    case "tree":
+        treeObj.treeKey(inputArr[1]);
+        break;
+    case "organize":
+        organizeObj.organizeKey(inputArr[1]);
+        break;
+    case "help":
+        helpObj.helpKey();
+        break;
+    default:
+        console.log("Please 🙏 Input Right command");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
